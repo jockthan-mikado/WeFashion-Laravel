dbe2e93cc1efb79229caa62852aad2bb40aa9a5d
jockthan <jockthanmik@gmail.com>
2022-06-25 13:44:28 +0200

    creation of the layouts folder and addition of files

--- WeFashion/resources/views/layouts/master.blade.php
+++ WeFashion/resources/views/layouts/master.blade.php
@@ -1,117 +1,45 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="img/favicon.png">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>WeFashion</title>
 
+    {{--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/album/">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- <link href="{{asset('css/app.css')}}" rel="stylesheet"> --}}
 
     <link rel="icon" href="{{asset('images/favicon.png')}}">
     <meta name="theme-color" content="#712cf9">
     <!-- Custom styles for this template -->
     <link href="css/album.css" rel="stylesheet">
-    <link href="css/tshirt.css" rel="stylesheet">
-
-
-
+    <link href="{{asset('css/tshirt.css')}}" rel="stylesheet">
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 
 </head>
-
 <body>
 
-    <header>
-        <div class="collapse bg-dark" id="navbarHeader">
 
-        </div>
-        <div class="navbar navbar-dark bg-dark box-shadow">
-            <div class="container d-flex justify-content-between">
-                <div class="d-flex ">
-
-                    <a href="#" class="navbar-brand d-flex align-items-center">
-                        <strong>WeFashion</strong>
-                    </a>
-                    <div class="navbar-nav d-flex">
-                        <div class="nav-item">
-                            <a class="nav-link" href="{{url('/')}}">Produit</a>
-                        </div>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('solde')}}">Categorie</a>
-                        </div>
-                       
-                    </div >
-                </div>
-
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="btn btn-primary btn-block" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                      document.getElementById('logout-form').submit();">
-                         Se déconnecter
-                     </a>
-
-                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                         @csrf
-                     </form>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-red">
-            <a class="navbar-brand" href="#"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produit</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Categorie</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-
-    <div class="content-wrapper">
-
-        <div class="content">
-            <div class="container-fluid">
-                @yield("contenu")
-            </div>
-        </div>
-
+    <div class="col-md-12">
+                @yield('barmenu')
     </div>
 
-    <!-- Control Sidebar -->
-    <!--On fait appelle au composant sidebar.php -->
 
-    <!-- /.control-sidebar -->
-
-    <footer class="main-footer">
-
-        <div class="float-right d-none d-sm-inline">
-            Anything you want
-        </div>
-
-        <strong>Copyright &copy; 2022 <a href="#">Mikado_Entreprise.com </a>.
-        </strong> All rights reserved.
-    </footer>
-</div>
-
-<script src="{{ mix('js/app.js') }}"></script>
+    <div class="col-md-12">
+        @yield('content')
+    </div>
 
 
-
+@section('scripts')
+<script src="{{asset('js/app.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+@show
 </body>
+
+    <div class="col-md-12">
+        @include('partials.footer')
+    </div>
 </html>
