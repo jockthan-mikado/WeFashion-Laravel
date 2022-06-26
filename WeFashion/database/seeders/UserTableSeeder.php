822b274d886bec8a84dc62da65a9a8b875eba717
jockthan <jockthanmik@gmail.com>
2022-06-26 23:40:29 +0200

    add readme

--- WeFashion/database/seeders/UserTableSeeder.php
+++ WeFashion/database/seeders/UserTableSeeder.php
@@ -17,11 +17,11 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::create([
-			'name'              => 'Jockthan',
-			'email'             => 'test@test.test',
+			'name'              => 'Edouard',
+			'email'             => 'Edouard@gmail.com',
 			'email_verified_at' => now(),
 			'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-			
+
 		]);
     }
 }
